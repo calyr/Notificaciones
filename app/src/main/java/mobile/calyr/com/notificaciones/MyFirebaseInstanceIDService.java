1d6c019bd66b814192f8686de9e5ba358ec46fe1
Roberto Carlos Callisaya Mamani <calyr_software@hotmail.com>
2017-10-10 23:58:43 -0400

    Initial commit

---
+++ app/src/main/java/mobile/calyr/com/notificaciones/MyFirebaseInstanceIDService.java
@@ -0,0 +1,8 @@
+package mobile.calyr.com.notificaciones;
+
+/**
+ * Created by robertocarloscallisayamamani on 10/10/17.
+ */
+
+public class MyFirebaseInstanceIDService {
+}
---
+++ app/src/main/java/mobile/calyr/com/notificaciones/MyFirebaseMessagingService.java
@@ -0,0 +1,31 @@
+package mobile.calyr.com.notificaciones;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by robertocarloscallisayamamani on 10/10/17.
+ */
+
+public class MyFirebaseMessagingService  extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
